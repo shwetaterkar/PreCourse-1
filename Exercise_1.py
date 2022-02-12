ae3c21f95eeb5a62029c83a38bb854584984c539
shwetaterkar <81641786+shwetaterkar@users.noreply.github.com>
2022-02-11 16:42:51 -0800

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,48 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :no
+
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+    
+     
+     
      def __init__(self):
+        self.arr = []
          
      def isEmpty(self):
-         
+        return self.arr == []
+    
      def push(self, item):
+        self.arr.append(item)
          
      def pop(self):
-        
+        if(not self.isEmpty()):
+           
+            element= self.arr[-1]
+            
+            del(self.arr[-1])
+            return element
+        else:
+            return "stack underflow"
         
      def peek(self):
+        return self.arr[-1]
         
      def size(self):
+        return len(self.arr)
          
      def show(self):
-         
-
+        print(self.arr) 
+        
 s = myStack()
 s.push('1')
 s.push('2')
 print(s.pop())
-print(s.show())
+s.show()
+         
+
+
